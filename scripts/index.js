1f8a1302caa268209eab7a2423b35973e2b4196d
SergyBIT1 <slavnij@gmail.com>
2024-07-04 16:56:20 +0300

    Создана функция добавления и удаления карточек

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,32 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCard(element) {
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
+  const cardDelete = cardElement.querySelector('.card__delete-button');
+  cardElement.querySelector('.card__image').src = element.link;
+  cardElement.querySelector('.card__title').textContent = element.name;
+  cardDelete.addEventListener('click', deleteCard);
+  return cardElement;
+};
 
 // @todo: Функция удаления карточки
+function deleteCard(evt) {
+  const evtTarget = evt.target.closest('.card');
+  evtTarget.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(element => {
+  placesList.append(createCard(element, deleteCard) )
+});
+
+
+
+
+
+
