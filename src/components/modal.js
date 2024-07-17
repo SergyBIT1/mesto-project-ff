b19d2e7b90634f45df59d37091cec554dd756950
SergyBIT1 <slavnij@gmail.com>
2024-07-17 17:34:28 +0300

    Открывается всплывающее окно редактирования профиля

--- src/components/modal.js
+++ src/components/modal.js
@@ -8,8 +8,18 @@ const profileEditButton = document.querySelector('.profile__edit-button');
 
 const openPopup = (popup) => {
   popup.classList.add('popup_is-opened')
-  // document.addEventListener('keydown', handleCloseEsc)
-  // document.addEventListener('click', modalWindowClose)
+  document.addEventListener('keydown', closeEsc)
 }
 
+const closePopup = (popup) => {
+  popup.classList.remove('popup_is-opened')
+  document.removeEventListener('keydown', closeEsc)
+}
 
+const closeEsc = (event) => {
+  const openedPopup = document.querySelector('.popup_is-opened');
+  if (event.key === 'Escape') {
+    closePopup(openedPopup)
+  }
+  
+} 
